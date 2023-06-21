b09f423f853d554a1619bce8a5a2fb03b20ea081
Karamjeet Singh <singhkaramjeetapple@gmail.com>
2023-06-20 19:49:50 -0700

    connection to postgres

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
-    'tags.apps.TagsConfig'
+    'tags.apps.TagsConfig',
+    'likes.apps.LikesConfig'
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoshop',
+        'USER': 'karamjeet',
+        'PASSWORD': 'karamjeet',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
