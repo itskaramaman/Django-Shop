1a97077ae195f57d3b28b29c2f11cb6b4fb229e5
Karamjeet Singh <singhkaramjeetapple@gmail.com>
2023-06-25 16:01:06 -0700

    added collections route

--- store/serializers.py
+++ store/serializers.py
@@ -3,18 +3,25 @@ from rest_framework import serializers
 from .models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title', 'products_count']
+
+    products_count = serializers.SerializerMethodField(method_name='get_product_count')
+
+    def get_product_count(self, collection: Collection):
+        return collection.product_set.count()
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(), view_name='collection-detail')
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+
+
--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('products/', views.product_list, name='home'),
     path('products/<int:id>/', views.product_detail, name='home'),
-     path('collections/<int:id>/', views.collection_detail, name='collection-detail')
+    path('collections/', views.collection_list, name='collection-list'),
+    path('collections/<int:pk>/', views.collection_detail, name='collection-detail')
 ]
